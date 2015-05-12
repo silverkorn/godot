60bdc322b7d082bf4e3a2eeb7098330a9ec5bddd
Silverkorn <silverkorn@gmail.com>
2015-05-12 18:24:34 -0400

    [Debug - Travis-CI] Another attempt with Travis-CI

---
+++ ci/travis-ci/dependencies.sh
@@ -0,0 +1,72 @@
+#!/usr/bin/env bash
+
+#***********************************************************************#
+#  dependencies.sh                                                      #
+#***********************************************************************#
+#                       This file is part of:                           #
+#                           GODOT ENGINE                                #
+#                    http://www.godotengine.org                         #
+#***********************************************************************#
+# Copyright (c) 2007-2015 Juan Linietsky, Ariel Manzur.                 #
+#                                                                       #
+# Permission is hereby granted, free of charge, to any person obtaining #
+# a copy of this software and associated documentation files (the       #
+# "Software"), to deal in the Software without restriction, including   #
+# without limitation the rights to use, copy, modify, merge, publish,   #
+# distribute, sublicense, and/or sell copies of the Software, and to    #
+# permit persons to whom the Software is furnished to do so, subject to #
+# the following conditions:                                             #
+#                                                                       #
+# The above copyright notice and this permission notice shall be        #
+# included in all copies or substantial portions of the Software.       #
+#                                                                       #
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,       #
+# EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF    #
+# MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.#
+# IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY  #
+# CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,  #
+# TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE     #
+# SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.                #
+#***********************************************************************#
+
+# Get platform from argument (Because of `sudo` restriction)
+PLATFORM=$1
+
+################
+# Dependencies #
+################
+
+# Use GCC 4.8+ for better C++11 support
+if ! [ "$PLATFORM" == "osx" ] && ! [ "$PLATFORM" == "iphone" ]; then 
+	add-apt-repository -y ppa:ubuntu-toolchain-r/test
+	apt-get -qq update
+	if [ "$CXX" = "g++" ]; then 
+		apt-get install -qq g++-4.8;
+	fi
+fi
+
+# Blackberry 10
+# *** TO FIX: Quit or accept licence agreement automatically
+#if [ "$PLATFORM" == "bb10" ] ]; then 
+#	# Around 400 MB download (Try to cache the directory: http://docs.travis-ci.com/user/caching/)
+#	export BB10_SDK_FILENAME=momentics-2.1.2-201503050937.linux.x86_64.run && wget --quiet -P /tmp/ https://developer.blackberry.com/native/downloads/fetch/$BB10_SDK_FILENAME
+#	bash /tmp/$BB10_SDK_FILENAME --quiet --nox11 <<END
+#	y
+#	END
+#	# TODO: Finish this part
+#fi
+
+# Javascript
+if [ "$PLATFORM" == "javascript" ]; then 
+	apt-get -qq install emscripten
+fi
+
+# Linux (incl. Server)
+if [ "$PLATFORM" == "x11" ] || [ "$PLATFORM" == "server" ]; then 
+	apt-get -qq install scons pkg-config libx11-dev libxcursor-dev build-essential libasound2-dev libfreetype6-dev libgl1-mesa-dev libglu-dev libssl-dev libxinerama-dev
+fi
+
+# Windows
+if [ "$PLATFORM" == "windows" ]; then 
+	apt-get -qq install mingw32 mingw-w64
+fi
---
+++ ci/travis-ci/prepare.sh
@@ -0,0 +1,60 @@
+#!/usr/bin/env bash
+
+#***********************************************************************#
+#  prepare.sh                                                           #
+#***********************************************************************#
+#                       This file is part of:                           #
+#                           GODOT ENGINE                                #
+#                    http://www.godotengine.org                         #
+#***********************************************************************#
+# Copyright (c) 2007-2015 Juan Linietsky, Ariel Manzur.                 #
+#                                                                       #
+# Permission is hereby granted, free of charge, to any person obtaining #
+# a copy of this software and associated documentation files (the       #
+# "Software"), to deal in the Software without restriction, including   #
+# without limitation the rights to use, copy, modify, merge, publish,   #
+# distribute, sublicense, and/or sell copies of the Software, and to    #
+# permit persons to whom the Software is furnished to do so, subject to #
+# the following conditions:                                             #
+#                                                                       #
+# The above copyright notice and this permission notice shall be        #
+# included in all copies or substantial portions of the Software.       #
+#                                                                       #
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,       #
+# EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF    #
+# MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.#
+# IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY  #
+# CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,  #
+# TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE     #
+# SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.                #
+#***********************************************************************#
+
+###############
+# Preparation #
+###############
+
+# Use GCC 4.8+ for better C++11 support
+if ! [ "$PLATFORM" == "osx" ] && ! [ "$PLATFORM" == "iphone" ]; then 
+	if [ "$CXX" = "g++" ]; then 
+		export CXX="g++-4.8" CC="gcc-4.8"
+	fi
+fi
+
+# Android
+if [ "$PLATFORM" == "android" ]; then 
+	#export ANDROID_HOME=
+	#export ANDROID_NDK_ROOT=
+	mkdir -p platform/android/java/libs/armeabi
+	mkdir -p platform/android/java/libs/x86
+fi
+
+# Javascript
+if [ "$PLATFORM" == "javascript" ]; then 
+	export EMSCRIPTEN_ROOT=$(em-config EMSCRIPTEN_ROOT)
+fi
+
+# Windows
+if [ "$PLATFORM" == "windows" ]; then 
+	#export MINGW32_PREFIX="/usr/lib/i586-mingw32msvc-"
+	#export MINGW64_PREFIX="/usr/lib/i686-w64-mingw32-"
+fi
